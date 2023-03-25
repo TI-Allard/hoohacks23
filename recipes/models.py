2f86d3b40948ff3496a8bd2b2ef156517ebf95a4
TI-Allard <allard.ti@gmail.com>
2023-03-25 17:34:50 -0400

    update recipes model and migrated

--- recipes/models.py
+++ recipes/models.py
@@ -4,3 +4,6 @@ from django.db import models
 
 class Recipes(models.Model):
     recipe_name = models.CharField(max_length=200)
+    ingredients = models.TextField(blank=True, null=True)
+    time=models.CharField(max_length=20, default = "")
+    steps = models.TextField(blank=True, null=True)
